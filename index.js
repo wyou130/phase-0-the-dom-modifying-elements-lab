1a45bd97e42f479c7b18271dc40c7fe547c26ff9
Wendy You <wyou130@gmail.com>
2022-07-10 20:29:35 -0400

    commit tree 17855f7cb488f5ef6552aac30c4ab767f28fef08

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// Write your code here!
+const main = document.getElementById("main")
+main.remove()
+
+const newHeader = document.createElement('h1')
+newHeader.id = "victory"
+newHeader.innerHTML = "Wendy is the champion"
